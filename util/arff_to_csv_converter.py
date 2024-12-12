b38ed26b95d1d1c4cdd803dac9d8b9037247c262
Mattia Ingrassia <tiaingra@gmail.com>
2024-12-12 22:54:04 +0100

    Delete util directory

--- util/arff_to_csv_converter.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-from scipy.io import arff
-
-input_arff_path = "data/dry_bean/dry_bean.arff"
-output_csv_path = "data/dry_bean/dry_bean.csv"
-
-data, meta = arff.loadarff(input_arff_path)
-dataframe = pd.DataFrame(data)
-dataframe.to_csv(output_csv_path, index=False, header=False)
